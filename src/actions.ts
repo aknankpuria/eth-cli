abf5af9b9e568d9d0038e81c515cccb7201ac13b
Oggy107 <urmalsingh10@gmail.com>
2023-02-21 17:15:47 +0530

    added: spinner

--- src/actions.ts
+++ src/actions.ts
@@ -1,8 +1,12 @@
 import { JsonRpcProvider, ethers } from "ethers";
+import ora, { Ora } from "ora";
+import logSymbols from "log-symbols";
+
 import config from "./config.js";
 
 export default class Action {
     private provider: JsonRpcProvider;
+    private spinner: Ora;
 
     constructor(network: string) {
         if (network == config.networks.goerli.name) {
@@ -14,26 +18,51 @@ export default class Action {
                 config.networks.mainnet.rpc_url
             );
         }
+
+        this.spinner = ora({ spinner: "dots5" });
     }
 
-    showBalance = async (address: string) => {
+    private startSpinner = (name: string): void => {
+        this.spinner.text = name;
+        this.spinner.start();
+    };
+
+    private stopSpinner = (symbol: string): void => {
+        this.spinner.stopAndPersist({
+            symbol,
+        });
+    };
+
+    showBalance = async (address: string): Promise<void> => {
+        this.startSpinner("fetching balance");
+
         try {
             const balance = (
                 await this.provider.getBalance(address)
             ).toString();
 
+            this.stopSpinner(logSymbols.success);
+
             console.log(`wei: ${balance}\neth: ${ethers.formatEther(balance)}`);
         } catch (error: any) {
+            this.stopSpinner(logSymbols.error);
+
             console.error(error.name, error.message);
         }
     };
 
-    showBlockNumber = async () => {
+    showBlockNumber = async (): Promise<void> => {
+        this.startSpinner("fetching block number");
+
         try {
             const block = (await this.provider.getBlockNumber()).toString();
 
+            this.stopSpinner(logSymbols.success);
+
             console.log(`Block Number : ${block}`);
         } catch (error: any) {
+            this.stopSpinner(logSymbols.error);
+
             console.error(error.name, error.message);
         }
     };
